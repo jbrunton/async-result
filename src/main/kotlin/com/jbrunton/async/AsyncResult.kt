cd937abe1a0ba0e485ed8c06142822092e12606d
John Brunton <jbrunton@zipcar.com>
2018-11-28 13:42:32 +0000

    add more docs

--- src/main/kotlin/com/jbrunton/async/AsyncResult.kt
+++ src/main/kotlin/com/jbrunton/async/AsyncResult.kt
@@ -3,26 +3,37 @@ package com.jbrunton.async
 import kotlin.reflect.KClass
 
 /**
- * A group of *members*.
+ * A discriminated union representing possible states in an asynchronous environment.
  *
- * This class has no useful logic; it's just a documentation example.
- *
- * @param T the type of a member in this group.
- * @property name the name of this group.
- * @constructor Creates an empty group.
+ * Possible values are [AsyncResult.Success], [AsyncResult.Loading] and [AsyncResult.Failure].
  */
 sealed class AsyncResult<out T> {
     /**
-     * A group of *members*.
+     * Represents a successful [AsyncResult] with a value of [value].
      *
-     * This class has no useful logic; it's just a documentation example.
-     *
-     * @param T the type of a member in this group.
-     * @property name the name of this group.
-     * @constructor Creates an empty group.
+     * @param T the type of the value of the result.
+     * @property value the successful result value.
+     * @constructor Creates a successful [AsyncResult].
      */
     data class Success<T>(val value: T): AsyncResult<T>()
+
+    /**
+     * Represents a loading [AsyncResult] state with an optional cached value of [cachedValue].
+     *
+     * @param T the type of the optional cached value.
+     * @property cachedValue the optional cached value.
+     * @constructor Creates a loading [AsyncResult].
+     */
     data class Loading<T>(val cachedValue: T? = null): AsyncResult<T>()
+
+    /**
+     * Represents an [AsyncResult] failure state.
+     *
+     * @param T the type of the optional cached value.
+     * @property error details of the error.
+     * @property cachedValue the optional cached value.
+     * @constructor Creates a failure [AsyncResult].
+     */
     data class Failure<T>(val error: Throwable, val cachedValue: T? = null): AsyncResult<T>()
 
     companion object {
@@ -33,7 +44,15 @@ sealed class AsyncResult<out T> {
 }
 
 
-
+/**
+ * Returns the value of the result.
+ *
+ * * For [AsyncResult.Success] this will be [value][AsyncResult.Success.value].
+ * * For [AsyncResult.Loading] this will be [cachedValue][AsyncResult.Loading.cachedValue] if it is not null. Otherwise
+ * a NullPointerException will be thrown.
+ * * For [AsyncResult.Failure] this will be [cachedValue][AsyncResult.Failure.cachedValue] if it is not null. Otherwise
+ * the [error][AsyncResult.Failure.error] will be thrown.
+ */
 fun <T> AsyncResult<T>.get(): T {
     return when (this) {
         is AsyncResult.Success -> this.value
@@ -42,6 +61,15 @@ fun <T> AsyncResult<T>.get(): T {
     }
 }
 
+/**
+ * Returns the value of the result if it has one and the given [defaultValue] otherwise.
+ *
+ * * For [AsyncResult.Success] this will be [value][AsyncResult.Success.value].
+ * * For [AsyncResult.Loading] this will be [cachedValue][AsyncResult.Loading.cachedValue] if it exists and
+ * [defaultValue] otherwise.
+ * * For [AsyncResult.Failure] this will be [cachedValue][AsyncResult.Failure.cachedValue] if it exists and
+ * [defaultValue] otherwise.
+ */
 fun <T> AsyncResult<T>.getOr(defaultValue: T): T {
     return when (this) {
         is AsyncResult.Success -> this.value
@@ -50,10 +78,22 @@ fun <T> AsyncResult<T>.getOr(defaultValue: T): T {
     }
 }
 
+/**
+ * Returns the value of the result if it exists and null otherwise.
+ *
+ * * For [AsyncResult.Success] this will be [value][AsyncResult.Success.value].
+ * * For [AsyncResult.Loading] this will be [cachedValue][AsyncResult.Loading.cachedValue] if it exists and
+ * null otherwise.
+ * * For [AsyncResult.Failure] this will be [cachedValue][AsyncResult.Failure.cachedValue] if it exists and
+ * null otherwise.
+ */
 fun <T> AsyncResult<T>.getOrNull(): T? {
     return getOr(null)
 }
 
+/**
+ * Returns [AsyncResult.Success] for any result which has a value (whether cached or not). Returns `this` otherwise.
+ */
 fun <T> AsyncResult<T>.useCachedValue(): AsyncResult<T> {
     val cachedValue = getOrNull()
     if (cachedValue == null) {
@@ -63,6 +103,9 @@ fun <T> AsyncResult<T>.useCachedValue(): AsyncResult<T> {
     }
 }
 
+/**
+ * Returns `this` if the result is [AsyncResult.Success]. Returns `other` otherwise.
+ */
 fun <T> AsyncResult<T>.or(other: AsyncResult<T>): AsyncResult<T> {
     return when (this) {
         is AsyncResult.Success -> this
@@ -70,6 +113,14 @@ fun <T> AsyncResult<T>.or(other: AsyncResult<T>): AsyncResult<T> {
     }
 }
 
+/**
+ * Returns the result of applying `transform` to the values of the results.
+ *
+ * Result types are as follows:
+ * * If both `this` and `other` are of type [AsyncResult.Success] then returns [AsyncResult.Success].
+ * * If either `this` or `other` are of type [AsyncResult.Loading] then returns [AsyncResult.Loading].
+ * * If either `this` or `other` are of type [AsyncResult.Failure] then returns [AsyncResult.Failure].
+ ]*/
 fun <S, T, U> AsyncResult<S>.zipWith(other: AsyncResult<T>, transform: (S, T) -> U): AsyncResult<U> {
     if (this is AsyncResult.Success && other is AsyncResult.Success) {
         return AsyncResult.Success(transform(this.value, other.value))
@@ -88,6 +139,9 @@ fun <S, T, U> AsyncResult<S>.zipWith(other: AsyncResult<T>, transform: (S, T) ->
     }
 }
 
+/**
+ * Returns a result of the same type as `this`, transforming the value.
+ */
 fun <S, T> AsyncResult<S>.map(transform: (S) -> T): AsyncResult<T> {
     return when (this) {
         is AsyncResult.Success -> AsyncResult.Success(transform(this.value))
@@ -96,6 +150,10 @@ fun <S, T> AsyncResult<S>.map(transform: (S) -> T): AsyncResult<T> {
     }
 }
 
+/**
+ * If `this` is `AsyncResult.Success` returns the result of applying `transform` to `this`. Otherwise just returns
+ * `this`.
+ */
 fun <T> AsyncResult<T>.onSuccess(transform: (AsyncResult.Success<T>) -> AsyncResult<T>): AsyncResult<T> {
     return when (this) {
         is AsyncResult.Success -> transform(this)
@@ -103,12 +161,21 @@ fun <T> AsyncResult<T>.onSuccess(transform: (AsyncResult.Success<T>) -> AsyncRes
     }
 }
 
+/**
+ * If `this` is `AsyncResult.Loading` returns the result of applying `transform` to `this`. Otherwise just returns
+ * `this`.
+ */
 fun <T> AsyncResult<T>.onLoading(transform: (AsyncResult.Loading<T>) -> AsyncResult<T>): AsyncResult<T> {
     return when (this) {
         is AsyncResult.Loading -> transform(this)
         else -> this
     }
 }
+
+/**
+ * If `this` is `AsyncResult.Failure` returns the result of applying `transform` to `this`. Otherwise just returns
+ * `this`.
+ */
 
 fun <T> AsyncResult<T>.onFailure(transform: (AsyncResult.Failure<T>) -> AsyncResult<T>): AsyncResult<T> {
     return when (this) {
@@ -117,24 +184,54 @@ fun <T> AsyncResult<T>.onFailure(transform: (AsyncResult.Failure<T>) -> AsyncRes
     }
 }
 
+/**
+ * If `this` is `AsyncResult.Success` then invokes `action` on `this`. Otherwise does nothing.
+ */
 fun <T> AsyncResult<T>.doOnSuccess(action: (AsyncResult.Success<T>) -> Unit): AsyncResult<T> {
     return onSuccess { action(it); it }
 }
 
+/**
+ * If `this` is `AsyncResult.Loading` then invokes `action` on `this`. Otherwise does nothing.
+ */
 fun <T> AsyncResult<T>.doOnLoading(action: (AsyncResult.Loading<T>) -> Unit): AsyncResult<T> {
     return onLoading { action(it); it }
 }
 
+/**
+ * If `this` is `AsyncResult.Failure` then invokes `action` on `this`. Otherwise does nothing.
+ */
 fun <T> AsyncResult<T>.doOnFailure(action: (AsyncResult.Failure<T>) -> Unit): AsyncResult<T> {
     return onFailure{ action(it); it }
 }
 
+/**
+ * Conditionally transforms [AsyncResult.Failure] results based on the value of [AsyncResult.Failure.error].
+ *
+ * For example, to transform a network failure:
+ *
+ *     result.onError(IOException::class) {
+ *         map { NoConnectionResult }
+ *     }
+ *
+ * You can also filter conditionally:
+ *
+ *     result.onError(HttpException::class) {
+ *         map { AuthFailureResult } whenever { it.code() == 401 }
+ *     }
+ *
+ * @param klass the error class to match on.
+ * @param handler the error handler to apply.
+ */
 fun <T, E: Throwable> AsyncResult<T>.onError(
         klass: KClass<E>,
-        block: ErrorHandler<T, E>.() -> Unit
-) = ErrorHandler(klass, this).apply(block).handle()
+        handler: ErrorHandler<T, E>.() -> Unit
+) = ErrorHandler<T, E>(klass).apply(handler).handle(this)
 
-class ErrorHandler<T, E: Throwable>(val klass: KClass<E>, val result: AsyncResult<T>) {
+/**
+ * DSL builder for error handling - see [AsyncResult.onError] for usage.
+ */
+class ErrorHandler<T, E: Throwable>(val klass: KClass<E>) {
     var filter: (E) -> Boolean = { true }
 
     infix fun whenever(filter: (E) -> Boolean) = apply {
@@ -147,7 +244,7 @@ class ErrorHandler<T, E: Throwable>(val klass: KClass<E>, val result: AsyncResul
         this.transform = transform
     }
 
-    fun handle(): AsyncResult<T> {
+    fun handle(result: AsyncResult<T>): AsyncResult<T> {
         when (result) {
             is AsyncResult.Failure -> {
                 if (klass.isInstance(result.error) && filter(result.error as E)) {
