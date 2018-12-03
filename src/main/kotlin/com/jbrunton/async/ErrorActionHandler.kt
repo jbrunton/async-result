9dc1b4b7b72897728731d2a64ea4eaff6bcdaf32
John Brunton <jbrunton@zipcar.co.uk>
2018-12-03 21:56:37 +0000

    extract error handler classes and remove some repetition

---
+++ src/main/kotlin/com/jbrunton/async/ErrorActionHandler.kt
@@ -0,0 +1,24 @@
+package com.jbrunton.async
+
+import kotlin.reflect.KClass
+
+/**
+ * DSL builder for error handling - see [AsyncResult.doOnError] for usage.
+ */
+class ErrorActionHandler<T, E: Throwable>(klass: KClass<E>) : AbstractErrorActionHandler<T, E>(klass) {
+    var action: (AsyncResult.Failure<T>) -> Unit = { _ -> }
+
+    infix fun action(action: (AsyncResult.Failure<T>) -> Unit) = apply {
+        this.action = action
+    }
+
+    fun handle(result: AsyncResult<T>) {
+        when (result) {
+            is AsyncResult.Failure -> {
+                if (klass.isInstance(result.error) && filter(result.error as E)) {
+                    action(result)
+                }
+            }
+        }
+    }
+}
